85587cf63e7f7476de60a1ce6fc090d42923be16
Matthew Thorley <matthew.thorley@lvt.com>
2022-03-15 13:11:35 -0600

    Make admin/messages the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  root to: 'admin/messages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
