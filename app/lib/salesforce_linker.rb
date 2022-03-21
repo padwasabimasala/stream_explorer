a9e169c8eec8df27310e06b278d1e8af061ed011
Matthew Thorley <matthew.thorley@lvt.com>
2022-03-21 17:11:26 -0600

    Parse links for Opportunity and Order

--- app/lib/salesforce_linker.rb
+++ app/lib/salesforce_linker.rb
@@ -1,3 +1,19 @@
 class SalesforceLinker
+  def initialize(message)
+    @message = message
+  end
+
+  def url_base
+    'https://lvt.lightning.force.com/lightning/r/'
+  end
+
+  def links 
+    if @message.stream_name && @message.stream_name.starts_with?('orderSfOpportunity')
+      return {'Opportunity' => url_base + 'Opportunity/' + @message.stream_name.split('-')[1] + '/view'}
+    end
+    if @message.data.dig('orderItems', 0, 'orderId')
+      return {'Order' => url_base + 'Order/' + @message.data.dig('orderItems', 0, 'orderId') + '/view'}
+    end
+  end
 
 end
