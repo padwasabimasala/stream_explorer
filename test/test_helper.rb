9e4bb44f66abcbd04029b074ba90efff653eac84
Matthew Thorley <matthew.thorley@lvt.com>
2022-03-21 15:56:01 -0600

    Remove unused db setup so tests run without database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
-require "rails/test_help"
+#Commented out to avoid attempting to reset the database when tests are run
+#require "rails/test_help"
+ActiveRecord::Base.maintain_test_schema = false
+
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  # parallelize(workers: :number_of_processors)
 end
